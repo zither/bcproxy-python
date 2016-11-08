6d614dddcd7d058779d8c4717a33be705dbbe642
Jun Zhou <ilorn.mc@gmail.com>
2016-11-08 21:26:58 +0800

    first commit

---
+++ bcprotocol.py
@@ -0,0 +1,214 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 –*- 
+# This is a simple BatMUD protocol parser, written using only the default python
+# library. If you want to make a suggestion or fix something you can contact-me
+# at ilorn.mc_at_gmail.com
+# Distributed over IDC(I Don't Care) license
+import sys
+import colortrans
+
+S_TEXT = 0
+S_ESC = 1
+S_TAG_OPEN = 2
+S_OPEN_CODE = 3
+S_TAG_CLOSE = 4
+S_CLOSE_CODE = 5
+S_AFTER_TEN = 6
+S_IAC = 7
+
+class Options:
+    def __init__(self, codes, enable_color, enable_combat_plugin):
+        self.codes = codes
+        self.enable_color = enable_color
+        self.enable_combat_plugin = enable_combat_plugin
+
+class Expression:
+    def __init__(self):
+        self.code = ""
+        self.argu = ""
+        self.content = ""
+
+class Parser:
+    def __init__(self, options):
+        self.options = options
+        self.stats = S_TEXT
+        self.output = ""
+        self.stack = []
+        # 当收到的数据不完整时，code 很可能被截断，导致前一位数字丢失
+        # 因此添加了 tmp_code 变量临时存储前一位，保证 code 始终为两位
+        self.tmp_code = ""
+        self.expression = None
+
+    def is_valid_number(self, letter):
+        return letter in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] 
+
+    def reset(self):
+        self.output = ""
+
+    def do_with_text(self, chars):
+        if self.expression:
+            self.expression.content += chars
+        else:
+            self.output += chars
+
+    def parse(self, data):
+        for index, char in enumerate(data):
+            if self.stats == S_TEXT:
+                if char == "\033":
+                    self.stats = S_ESC
+                else:
+                    self.do_with_text(char)
+                continue
+
+            if self.stats == S_ESC:
+                if char == "<":
+                    self.stats = S_TAG_OPEN
+                elif char == ">":
+                    self.stats = S_TAG_CLOSE
+                elif char == "|" and self.expression:
+                    self.expression.argu = self.expression.content
+                    self.expression.content = ""
+                    self.stats = S_TEXT
+                else:
+                    self.do_with_text("\033" + char)
+                    self.stats = S_TEXT
+                continue
+
+            if self.stats == S_TAG_OPEN:
+                if self.is_valid_number(char):
+                    self.tmp_code += char
+                    self.stats = S_OPEN_CODE
+                else:
+                    self.do_with_text("\033" + "<" + char)
+                    self.stats = S_TEXT
+                continue
+
+            if self.stats == S_OPEN_CODE:
+                if self.is_valid_number(char):
+                    if self.expression:
+                        self.stack.append(self.expression)
+                    self.expression = Expression()
+                    self.expression.code += self.tmp_code + char
+                    self.stats = S_TEXT
+                else:
+                    self.do_with_text("\033" + "<" + self.tmp_code + char)
+                    self.stats = S_TEXT
+                self.tmp_code = ""
+                continue
+
+            if self.stats == S_TAG_CLOSE:
+                if self.is_valid_number(char):
+                    self.tmp_code += char
+                    self.stats = S_CLOSE_CODE
+                else:
+                    self.do_with_text("\033" + ">" + char)
+                    self.stats = S_TEXT
+                continue
+            
+            if self.stats == S_CLOSE_CODE:
+                if self.is_valid_number(char):
+                    code = self.tmp_code + char
+                    if not self.expression or self.expression.code != code:
+                        self.stats = S_TEXT
+                    elif self.expression.code == "10" and self.expression.argu == "spec_prompt":
+                        self.stats = S_AFTER_TEN
+                    elif not self.stack:
+                        self.output += self.parse_exp(self.expression)
+                        self.expression = None
+                        self.stats = S_TEXT
+                    else:
+                        closed_expression = self.expression
+                        parent_expression = self.stack.pop()
+                        parent_expression.content += self.parse_exp(closed_expression)
+                        self.expression = parent_expression
+                        self.stats = S_TEXT
+                else:
+                    self.do_with_text("\033" + ">" + self.tmp_code + char)
+                    self.stats = S_TEXT
+                self.tmp_code = ""
+                continue
+
+            if self.stats == S_AFTER_TEN:
+                if char == "\377":
+                    self.stats = S_IAC
+                elif char == "\033":
+                    self.expression = None
+                    self.stats = S_ESC
+                else:
+                    self.expression = None
+                    self.do_with_text(char)
+                    self.stats = S_TEXT
+                continue
+
+            if self.stats == S_IAC:
+                if char == "\371":
+                    self.output += self.parse_exp(self.expression)
+                    self.expression = None
+                    self.stats = S_TEXT
+                else:
+                    if self.stack:
+                        self.expression = self.stack.pop()
+                    else:
+                        self.expression = None
+                    self.do_with_text("\377" + char)
+                    self.stats = S_TEXT
+                continue
+
+        return self.output
+
+    def parse_exp(self, exp):
+        if exp.code in self.options.codes or exp.code in ["05", "06", "11", "29", "40", "41", "42"]:
+            return ""
+
+        if exp.code in ["22", "23", "24", "25", "31"]:
+            return exp.content
+
+        if exp.code == "10":
+            if exp.argu == "spec_battle" and self.options.enable_combat_plugin:
+                return "[-10-]" + exp.content.replace("\n", " ").strip() + "\r\n"
+            elif exp.argu == "spec_prompt":
+                return exp.content.strip() +"\r\n"
+            elif exp.content == "NoMapSupport":
+                return ""
+            else:
+                return exp.content.strip() +"\r\n"
+               
+        if exp.code == "20" or exp.code == "21":
+            if not self.options.enable_color:
+                return exp.content
+            elif exp.argu and len(exp.argu) == 6:
+                short, rgb = colortrans.rgb2short(exp.argu)
+                return "\033[{}8;5;{}m{}\033[0m".format(3 if exp.code == "20" else 4, short, exp.content)
+            elif exp.argu and len(exp.argu) > 0 and len(exp.argu) < 6:
+                rgb = exp.argu.zfill(6)
+                short, _ = colortrans.rgb2short(rgb)
+                return "\033[{}8;5;{}m{}\033[0m".format(3 if exp.code == "20" else 4, short, exp.content)
+
+        if exp.code == "99":
+            if exp.content.startswith("BAT_MAPPER;;REALM_MAP"):
+                return "Exited to realm map.\r\n"
+            elif exp.content.startswith("BAT_MAPPER;;"):
+                room = exp.content.split(";;")
+                return "[-{}-]{};;{};;{};;{};;{}\r\n".format(exp.code, room[1], room[2], room[3], room[4], room[7])
+
+        return "[-{}-]{}\r\n".format(exp.code, exp.content)
+
+
+if __name__ == '__main__':
+    options = Options([], True, True)
+    parser = Parser(options)
+
+    with open("logs_debug.txt") as logs:
+        content = logs.read()
+    #content = """<10spec_map| yest [1m<31north|north>31>10"""
+    #content = "<10spec_map|<31north|north>31>10"
+    #content = "<10spec_map|123>10"
+    #content = """<10spec_map|<200|[32mT>20>10"""
+    #content = "<540 0 0>54<10spec_prompt|Hp:301/301 Sp:948/948 Ep:245/245 Exp:147871 >>10<52Zjmee 0 duck 65 1 147871>52<50301 301 948 948 245 245>50"
+    #content = """<10spec_map| <200000FF|>20  Massive bronze gate (closed) leads north.>10"""
+    #content = """123>20<10spec_map| <200000FF|>20  Massive bronze gate (closed) leads north.>10""" 
+    #content = """<10spec_map|<200000FF|| >20>20 DATA>20<770000AA|>77  GOOD>10"""
+    #content = "<10spec_prompt|Hp:301/301 Sp:963/963 Ep:241/241 Exp:1741 >>10BAAAA"
+
+    data = parser.parse(content)
+    print data
---
+++ proxy.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 –*- 
+# This is a simple port-forward / proxy, written using only the default python
+# library. If you want to make a suggestion or fix something you can contact-me
+# at voorloop_at_gmail.com
+# Distributed over IDC(I Don't Care) license
+import socket
+import select
+import time
+import sys
+import bcprotocol
+
+# Changing the buffer_size and delay, you can improve the speed and bandwidth.
+# But when buffer get to high or delay go too down, you can broke things
+buffer_size = 4096
+delay = 0.0001
+forward_to = ('batmud.bat.org', 23)
+
+class RemoteParser:
+    def parse(self, data):
+        return data
+
+class LocalParser (RemoteParser):
+    def __init__(self, options):
+        self.bc_parser = bcprotocol.Parser(options)
+
+    def parse(self, data):
+        #with open("logs_debug.txt", 'ab') as logs:
+        #    logs.write(data)
+        self.bc_parser.reset()
+        return self.bc_parser.parse(data) 
+
+class Forward:
+    def __init__(self):
+        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, port):
+        try:
+            self.forward.connect((host, port))
+            return self.forward
+        except Exception, e:
+            print e
+            return False
+
+class TheServer:
+    input_list = []
+    channel = {}
+    parser = {}
+
+    def __init__(self, host, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((host, port))
+        self.server.listen(200)
+
+    def main_loop(self):
+        self.input_list.append(self.server)
+        while 1:
+            time.sleep(delay)
+            ss = select.select
+            inputready, outputready, exceptready = ss(self.input_list, [], [])
+            for self.s in inputready:
+                if self.s == self.server:
+                    self.on_accept()
+                    break
+
+                self.data = self.s.recv(buffer_size)
+                if len(self.data) == 0:
+                    self.on_close()
+                    break
+                else:
+                    self.on_recv()
+                
+                
+    def on_accept(self):
+        forward = Forward().start(forward_to[0], forward_to[1])
+        clientsock, clientaddr = self.server.accept()
+        if forward:
+            print clientaddr, "has connected"
+            self.input_list.append(clientsock)
+            self.input_list.append(forward)
+            self.channel[clientsock] = forward
+            self.channel[forward] = clientsock
+
+            self.parser[clientsock] = RemoteParser()
+            self.parser[forward] = LocalParser(options)
+
+            forward.send("\033bc 1\n")
+        else:
+            print "Can't establish connection with remote server.",
+            print "Closing connection with client side", clientaddr
+            clientsock.close()
+
+    def on_close(self):
+        print self.s.getpeername(), "has disconnected"
+        #remove objects from input_list
+        self.input_list.remove(self.s)
+        self.input_list.remove(self.channel[self.s])
+        out = self.channel[self.s]
+        # close the connection with client
+        self.channel[out].close()  # equivalent to do self.s.close()
+        # close the connection with remote server
+        self.channel[self.s].close()
+        # delete both objects from channel dict
+        del self.channel[out]
+        del self.channel[self.s]
+
+    def on_recv(self):
+        data = self.data
+        # here we can parse and/or modify the data before send forward
+        new_data = self.parser[self.s].parse(data)
+        self.channel[self.s].sendall(new_data)
+
+
+if __name__ == '__main__':
+    ################
+    # User options #
+    ################
+    port = 9999
+    codes = ["50", "52", "53", "54", "60", "61", "62", "63", "64", "70"]
+    enable_color = True
+    enable_combat_plugin = True
+
+    try:
+        options = bcprotocol.Options(codes, enable_color, enable_combat_plugin)
+        server = TheServer('', port)
+        print "Bat proxy is running at {}:{}".format("localhost", port)
+        server.main_loop()
+    except KeyboardInterrupt:
+        print "Ctrl C - Stopping server"
+
+sys.exit(1)
